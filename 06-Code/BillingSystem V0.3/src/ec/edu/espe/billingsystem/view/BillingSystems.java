1d74d329d733e34138ba85048e12c894bdd58c22
Jenniffer Paola Marquez Quiroz <marquezqjenniffer@gmail.com>
2024-06-16 08:49:46 -0500

    Update BillingSystems.java

--- 06-Code/BillingSystem V0.3/src/ec/edu/espe/billingsystem/view/BillingSystems.java
+++ 06-Code/BillingSystem V0.3/src/ec/edu/espe/billingsystem/view/BillingSystems.java
@@ -3,41 +3,57 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ec.edu.espe.billingsystem.view;
-
 import ec.edu.espe.billingsystem.model.Product;
+import ec.edu.espe.billingsystem.utils.InputUtils;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Scanner;
 /**
  *
  * @author David Pantoja, JavaSquad, DCCO-ESPE
  */
 public class BillingSystems {
     public static void main(String[] args) {
-     int number=0;
-    do {
-     System.out.println("Program startup \n1: Data entry \n2:Food menu \n3: Exit");
-        number =Integer.parseInt(System.console().readLine());
-    switch(number){
-        case 1:
-        FileManager.addBilling();
-        break;
-        case 2:
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        
+        do {
+            showMenu();
+            number = InputUtils.getInt("Choose an option:");
+            
+            switch (number) {
+                case 1:
+                    FileManager.addBilling();
+                    break;
+                case 2:
+                    showFoodMenu();
+                    break;
+                case 3:
+                    System.out.println("Exiting the program. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
+        } while (number != 3);
+        
+        scanner.close();
+    }
+    
+    private static void showMenu() {
+        System.out.println("Program startup");
+        System.out.println("1: Data entry");
+        System.out.println("2: Food menu");
+        System.out.println("3: Exit");
+    }
+
+    private static void showFoodMenu() {
         List<Product> products = new ArrayList<>();
         products.add(new Product(1, "Pizza", 5.99, 100));
         products.add(new Product(2, "Fries", 2.99, 200));
         products.add(new Product(3, "Soda", 1.49, 300));
+        
         for (Product product : products) {
-        System.out.println(product.getId() + ": " + product.getName() + " - $" + product.getPrice());
+            System.out.println(product.getId() + ": " + product.getName() + " - $" + product.getPrice());
         }
-        break;
-        case 3:
-        break;
-        default:   
-       System.out.println("Invalid option");
-    }
-    
-    
-    }while(number!=3);   
     }
 }
