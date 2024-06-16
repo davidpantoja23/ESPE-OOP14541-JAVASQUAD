d9c826a3d168c0739d75b3470dc2589dfc7fa244
Jenniffer Paola Marquez Quiroz <marquezqjenniffer@gmail.com>
2024-06-15 19:14:31 -0500

    Update BillingManager.java

--- 06-Code/BillingSystem V0.3/src/ec/edu/espe/billingsystem/controller/BillingManager.java
+++ 06-Code/BillingSystem V0.3/src/ec/edu/espe/billingsystem/controller/BillingManager.java
@@ -18,20 +18,29 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class BillingManager {
     public Invoice createInvoice(Customer customer, List<Product> products, PaymentMethod paymentMethod) {
+        Objects.requireNonNull(customer, "Customer cannot be null");
+        Objects.requireNonNull(products, "Products cannot be null");
+        Objects.requireNonNull(paymentMethod, "Payment method cannot be null");
         Invoice invoice = new Invoice(customer, paymentMethod);
         for (Product product : products) {
-            invoice.addLine(new InvoiceLine(product, product.getStock()));
+            if (product != null && product.getStock() > 0) {
+                invoice.addLine(new InvoiceLine(product, product.getStock()));
+            }
         }
         return invoice;
     }
 
     public void saveInvoiceToFile(Invoice invoice, String fileName) {
+        Objects.requireNonNull(invoice, "Invoice cannot be null");
+        Objects.requireNonNull(fileName, "File name cannot be null");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             writer.write(invoice.toString());
         } catch (IOException e) {
+            System.err.println("An error occurred while saving the invoice to the file: " + e.getMessage());
             e.printStackTrace();
         }
     }
