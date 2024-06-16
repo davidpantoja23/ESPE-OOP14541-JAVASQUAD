12b47838a38f7a3d1f1d5e1c4b1057f59d2b4152
Jenniffer Paola Marquez Quiroz <marquezqjenniffer@gmail.com>
2024-06-15 19:22:58 -0500

    Update InventoryManager.java

--- 06-Code/BillingSystem V0.3/src/ec/edu/espe/billingsystem/controller/InventoryManager.java
+++ 06-Code/BillingSystem V0.3/src/ec/edu/espe/billingsystem/controller/InventoryManager.java
@@ -4,27 +4,25 @@ package ec.edu.espe.billingsystem.controller;
 import ec.edu.espe.billingsystem.model.Inventory;
 import ec.edu.espe.billingsystem.model.Product;
 
+import java.util.Objects;
+
 /**
  *
  * @author David Pantoja, JavaSquad, DCCO-ESPE
  */
 
-
 public class InventoryManager {
     private final Inventory inventory;
-
     public InventoryManager(Inventory inventory) {
-        this.inventory = inventory;
+        this.inventory = Objects.requireNonNull(inventory, "Inventory cannot be null");
     }
-
     public void addProduct(Product product) {
+        Objects.requireNonNull(product, "Product cannot be null");
         inventory.addProduct(product);
     }
-
     public void removeProduct(int productId) {
         inventory.removeProduct(productId);
     }
-
     public Inventory getInventory() {
         return inventory;
     }
