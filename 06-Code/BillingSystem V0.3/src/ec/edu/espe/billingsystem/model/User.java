e38b742986899623789f405f3cffbc81c6af5157
SimoneMedina <auquillamedina.simone@gmail.com>
2024-06-25 08:47:45 -0500

    now

---
+++ 06-Code/BillingSystem V0.3/src/ec/edu/espe/billingsystem/model/User.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.billingsystem.model;
+
+/**
+ *
+ * @author Simone Medina, JavaSquad, DCCO-ESPE
+ */
+
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean authenticate(String password) {
+        return this.password.equals(password);
+    }
+}
