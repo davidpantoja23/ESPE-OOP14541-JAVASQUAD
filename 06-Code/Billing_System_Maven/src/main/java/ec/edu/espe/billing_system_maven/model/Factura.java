669d5503c32ffbeed62fefcdbb975f557b231366
Carlos David Pantoja Jiménez <davidpantoja2311@hotmail.com>
2024-07-29 12:09:15 -0500

    Update Factura.java

--- 06-Code/Billing_System_Maven/src/main/java/ec/edu/espe/billing_system_maven/model/Factura.java
+++ 06-Code/Billing_System_Maven/src/main/java/ec/edu/espe/billing_system_maven/model/Factura.java
@@ -1,14 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.espe.billing_system_maven.model;
 
 import org.bson.Document;
 
 /**
  *
- * @author Jenniffer Marquez, Javasquad, DCCO-ESPE
+ * @author Javasquad, DCCO-ESPE
  */
 public class Factura {
     private String numeroFactura;
